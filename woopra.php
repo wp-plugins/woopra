6ccf1b728116d1d1d066b3b6acb6724587e99119
eliekhoury <eliekhoury@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-09 00:51:48 +0000

    Minor update
    git-svn-id: https://plugins.svn.wordpress.org/woopra/trunk@63633 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woopra.php
+++ woopra.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.woopra.com
 Description: This plugin adds Woopra's real-time analytics to any WordPress installation.  Simply sign up at Woopra.com, then activate the plugin and configure your site ID in the Woopra settings.
 Version: 1.3.4
 Author: Elie El Khoury
-Author URI: http://www.woopra.com
+Author URI: http://www.ekhoury.com
 */
 
 
