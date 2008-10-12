708ef57fb21104d62fb478cb4c0f32c606b06a3b
ShaneF <ShaneF@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-12 13:46:05 +0000

    translation feature to the plugin.
    git-svn-id: https://plugins.svn.wordpress.org/woopra/trunk@68683 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woopra_analytics.php
+++ woopra_analytics.php
@@ -37,9 +37,9 @@ if (!woopra_check_analytics()) {
 	<div id="woopra_analytics_box">
 		
 		<div class="woptions">
-		<a href="#" onclick="return refreshCurrent();">Refresh</a>
+		<a href="#" onclick="return refreshCurrent();"><?php _e('Refresh') ?></a>
 		&nbsp;-&nbsp;
-		<a id="daterangelink" href="#" onclick="return showDatePicker();" title="Click here to change the date range"><script type="text/javascript">document.write(getDateLinkText())</script></a>
+		<a id="daterangelink" href="#" onclick="return showDatePicker();" title="<?php _e('Click here to change the date range') ?>"><script type="text/javascript">document.write(getDateLinkText())</script></a>
 			<div id="datepickerdiv">
 				<table><tr>
 				<td align="center">From: <input type="text" class="w8em format-y-m-d divider-dash highlight-days-12 no-fade" id="dp-from" name="dp-from" value="" maxlength="10" /></td>
@@ -47,8 +47,8 @@ if (!woopra_check_analytics()) {
 				</tr>
 				<tr>
 				<td colspan="2" style="padding-top: 5px; text-align: right;">
-				<input value="Cancel" name="approveit" class="button-secondary" type="submit" onclick="return closeDatePicker();">
-				<input value="Apply Date Range" name="approveit" class="button-secondary" type="submit" onclick="return applyDatePicker();">
+				<input value="<?php _e('Cancel') ?>" name="approveit" class="button-secondary" type="submit" onclick="return closeDatePicker();">
+				<input value="<?php _e('Apply Date Range') ?>" name="approveit" class="button-secondary" type="submit" onclick="return applyDatePicker();">
 				</td>
 				</tr>
 				</table>
@@ -64,42 +64,42 @@ if (!woopra_check_analytics()) {
 	<script type="text/javascript">
 	
 	woopra_website = '<?php echo get_option("siteurl"); ?>';
+		
+	addSuperTab('<?php _e("Visitors") ?>','visitors');
+	addSuperTab('<?php _e("Systems") ?>','systems');
+	addSuperTab('<?php _e("Pages") ?>','pages');
+	addSuperTab('<?php _e("Referrers") ?>','referrers');
+	addSuperTab('<?php _e("Searches") ?>','searches');
 	
-	addSuperTab('Visitors','visitors');
-	addSuperTab('Systems','systems');
-	addSuperTab('Pages','pages');
-	addSuperTab('Referrers','referrers');
-	addSuperTab('Searches','searches');
+	addSubTab('<?php _e("Overview") ?>', 'overview', 'visitors', 'GET_GLOBALS');
+	addSubTab('<?php _e("Countries") ?>', 'countries', 'visitors', 'GET_COUNTRIES');
+	addSubTab('<?php _e("Tagged Visitors") ?>', 'taggedvisitors', 'visitors', 'GET_SPECIALVISITORS');
+	addSubTab('<?php _e("Bounce Rate") ?>', 'bounces', 'visitors', 'GET_VISITBOUNCES');
+	addSubTab('<?php _e("Visit Durations") ?>', 'durations', 'visitors', 'GET_VISITDURATIONS');
 	
-	addSubTab('Overview', 'overview', 'visitors', 'GET_GLOBALS');
-	addSubTab('Countries', 'countries', 'visitors', 'GET_COUNTRIES');
-	addSubTab('Tagged Visitors', 'taggedvisitors', 'visitors', 'GET_SPECIALVISITORS');
-	addSubTab('Bounce Rate', 'bounces', 'visitors', 'GET_VISITBOUNCES');
-	addSubTab('Visit Durations', 'durations', 'visitors', 'GET_VISITDURATIONS');
+	addSubTab('<?php _e("Browsers") ?>', 'browsers', 'systems', 'GET_BROWSERS');
+	addSubTab('<?php _e("Platforms") ?>', 'platforms', 'systems', 'GET_PLATFORMS');
+	addSubTab('<?php _e("Screen Resolutions") ?>', 'resolutions', 'systems', 'GET_RESOLUTIONS');
+	addSubTab('<?php _e("Languages") ?>', 'languages', 'systems', 'GET_LANGUAGES');
 	
-	addSubTab('Browsers', 'browsers', 'systems', 'GET_BROWSERS');
-	addSubTab('Platforms', 'platforms', 'systems', 'GET_PLATFORMS');
-	addSubTab('Screen Resolutions', 'resolutions', 'systems', 'GET_RESOLUTIONS');
-	addSubTab('Languages', 'languages', 'systems', 'GET_LANGUAGES');
+	addSubTab('<?php _e("Pageviews") ?>', 'pageviews', 'pages', 'GET_PAGEVIEWS');
+	addSubTab('<?php _e("Landing Pages") ?>', 'landing', 'pages', 'GET_PAGELANDINGS');
+	addSubTab('<?php _e("Exit Pages") ?>', 'exit', 'pages', 'GET_PAGEEXITS');
+	addSubTab('<?php _e("Outgoing Links") ?>', 'outgoing', 'pages', 'GET_OUTGOINGLINKS');
+	addSubTab('<?php _e("Downloads") ?>', 'downloads', 'pages', 'GET_DOWNLOADS');
 	
-	addSubTab('Pageviews', 'pageviews', 'pages', 'GET_PAGEVIEWS');
-	addSubTab('Landing Pages', 'landing', 'pages', 'GET_PAGELANDINGS');
-	addSubTab('Exit Pages', 'exit', 'pages', 'GET_PAGEEXITS');
-	addSubTab('Outgoing Links', 'outgoing', 'pages', 'GET_OUTGOINGLINKS');
-	addSubTab('Downloads', 'downloads', 'pages', 'GET_DOWNLOADS');
+	addSubTab('<?php _e("Referrer Types") ?>', 'reftypes', 'referrers', 'GET_REFERRERTYPES');
+	addSubTab('<?php _e("Regular Referrers") ?>', 'refdefault', 'referrers', 'GET_REFERRERS&type=DEFAULT');
+	addSubTab('<?php _e("Search Engines") ?>', 'refsearch', 'referrers', 'GET_REFERRERS&type=SEARCHENGINES');
+	addSubTab('<?php _e("Feed Readers") ?>', 'reffeeds', 'referrers', 'GET_REFERRERS&type=FEEDS');
+	addSubTab('<?php _e("Emails") ?>', 'refmails', 'referrers', 'GET_REFERRERS&type=MAILS');
+	addSubTab('<?php _e("Social Bookmarks") ?>', 'refbookmarks', 'referrers', 'GET_REFERRERS&type=SOCIALBOOKMARKS');
+	addSubTab('<?php _e("Social Networks") ?>', 'refnetworks', 'referrers', 'GET_REFERRERS&type=SOCIALNETWORKS');
+	addSubTab('<?php _e("Media") ?>', 'refmedia', 'referrers', 'GET_REFERRERS&type=MEDIA');
+	addSubTab('<?php _e("News") ?>', 'refnews', 'referrers', 'GET_REFERRERS&type=NEWS');
 	
-	addSubTab('Referrer Types', 'reftypes', 'referrers', 'GET_REFERRERTYPES');
-	addSubTab('Regular Referrers', 'refdefault', 'referrers', 'GET_REFERRERS&type=DEFAULT');
-	addSubTab('Search Engines', 'refsearch', 'referrers', 'GET_REFERRERS&type=SEARCHENGINES');
-	addSubTab('Feed Readers', 'reffeeds', 'referrers', 'GET_REFERRERS&type=FEEDS');
-	addSubTab('Emails', 'refmails', 'referrers', 'GET_REFERRERS&type=MAILS');
-	addSubTab('Social Bookmarks', 'refbookmarks', 'referrers', 'GET_REFERRERS&type=SOCIALBOOKMARKS');
-	addSubTab('Social Networks', 'refnetworks', 'referrers', 'GET_REFERRERS&type=SOCIALNETWORKS');
-	addSubTab('Media', 'refmedia', 'referrers', 'GET_REFERRERS&type=MEDIA');
-	addSubTab('News', 'refnews', 'referrers', 'GET_REFERRERS&type=NEWS');
-	
-	addSubTab('Search Queries', 'queries', 'searches', 'GET_QUERIES');
-	addSubTab('Keywords', 'keywords', 'searches', 'GET_KEYWORDS');
+	addSubTab('<?php _e("Search Queries") ?>', 'queries', 'searches', 'GET_QUERIES');
+	addSubTab('<?php _e("Keywords") ?>', 'keywords', 'searches', 'GET_KEYWORDS');
 	
 	setCurrentSuperTab('visitors');
 	</script>
