ae1013ab38941d1bb832a973deb1fac1f41dd7e9
ShaneF <ShaneF@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-01 22:30:46 +0000

    first draft of the new design code for the woopra wordpress login
    git-svn-id: https://plugins.svn.wordpress.org/woopra/trunk@131333 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woopra.php
+++ woopra.php
@@ -1,284 +1,255 @@
-<?php
-/*
-Plugin Name: Woopra
-Plugin URI: http://www.woopra.com
-Description: This plugin adds Woopra's real-time analytics to any WordPress installation.  Simply sign up at Woopra.com, then activate the plugin!
-Version: 1.4.1
-Author: Elie El Khoury
-Author URI: http://www.ekhoury.com
-*/
+<?php /*
 
-load_plugin_textdomain('woopra', PLUGINDIR.'/'.dirname(plugin_basename(__FILE__)) . '/locale');
+**************************************************************************
 
-function woo_session_start() {
-	global $woopra_events;
+Plugin Name:  Woopra
+Plugin URI:   http://www.woopra.com
+Version:      1.4.1
+Description:  This plugin adds Woopra's real-time analytics to any WordPress installation.  Simply sign up at Woopra.com, then activate the plugin!
+Author:       <a href="http://www.ekhoury.com">Elie El Khoury</a>, <a href="http://bugssite.org">Shane Froebel</a>
+Author URI:   http://www.woopra.com/
 
-	if (!isset($_SESSION)) {
- 		session_start();
-	}
+**************************************************************************/
 
-	if (isset($_SESSION['temp_woopra_comment'])) {
-		$comment_id = $_SESSION['temp_woopra_comment'];	
-		$tmp_comment = get_comment($comment_id);
-		$woopra_events['Comment'] = $tmp_comment->comment_content;
-		unset($_SESSION['temp_woopra_comment']);
+/**
+ * @author Elie El Khoury, Shane Froebel
+ * @copyright 2008
+ */
+class Woopra {
+
+	/**
+	 * @var $woopra_events Holds all the events and their descriptions.
+	 * @since 1.4.1
+	 * @see __construct()
+	 */
+	var $woopra_events = array();
+	
+	/**
+	 * @var $woopra_actionable_events Holds events that are fully "acceptable" to be passed onto the application.
+	 * @since 1.4.1
+	 */
+	var $woopra_actionable_events = array();
+	
+	/**
+	 * @var $woopra_vistors Current vistor information on the site are stored in this varabile.
+	 * @since 1.4.1
+	 */
+	var $woopra_vistors = array();
+
+	/** DEBUG VAR **/
+	var $debug = false;
+	
+	/**
+	 * Contructor Class
+	 * @since 1.4.1
+	 * @return
+	 * @constructor
+	 */
+	function __construct() {
+		
+		//	Load Translation Files
+		load_plugin_textdomain( 'woopra', false, '/woopra/locale' );
+		
+		$this->woopra_events['main'] = array(
+			'comment_post' => array(
+				'label' => _("Posted Comments"),
+				'descp' => _("Everytime a user posts a comment you will see an event in the application."),
+			),
+		);
+		
+		//	Filters
+		
+		
+		//	Actions
+		add_action( 'admin_menu',               array(&$this, 'register_settings_page') );
+		add_action(	'template_redirect',		array(&$this, 'woopra_detect')	);
+		add_action( 'wp_footer', 				array(&$this, 'woopra_widget') );
+		
+		//	Initilize Action Code
+		/*
+		$all_events = array_merge($this->woopra_events['main'], $this->woopra_events['admin']);
+		foreach ($all_events as $event_name) {
+			add_action( $event_name, 			array(&$this, $this->process_event) );
+		}
+		*/
+		
 	}
 	
-	if (get_option('woopra_show_searches')=='YES') {
-		if (isset($_GET['s'])) {
-			$woopra_events['Search'] = $_GET['s']; 
-		}
+	/**
+	 * Regestration of the Setting Page
+	 * @return 
+	 */
+	function register_settings_page() {
+		add_options_page( __('Woopra Settings', 'woopra'), __("Woopra Settings", 'woopra'), 'manage_options', 'woopra', array(&$this, 'settings_page') );
+	}
+
+	/**
+	 * The setting page itself.
+	 * @return 
+	 */
+	function settings_page() { ?>
 	
-		if (isset($_GET['q'])) {
-			$woopra_events['Search'] = $_GET['q']; 
-		}
-	}
-}
-
-function woo_detect() {
-    global $woopra_visitor;
-
-    $author = str_replace("\"","\\\"",$_COOKIE['comment_author_'.COOKIEHASH]);
-    $email = str_replace("\"","\\\"",$_COOKIE['comment_author_email_'.COOKIEHASH]);
-    if (!empty($author)) {
-        $woopra_visitor['name'] = $author;
-        $woopra_visitor['email'] = $email;
-    }
-
-    if (is_user_logged_in()) {
-        global $userdata;
-        get_currentuserinfo();
-        $woopra_visitor['name'] = $userdata->display_name;
-        $woopra_visitor['email'] = $userdata->user_email;
-    }
-}
-
-function woo_comment($comment_id) {
-	if (get_option('woopra_show_comments')=='YES') {
-		if (!isset($_SESSION)) {
- 			session_start();
-		}
-		$_SESSION['temp_woopra_comment'] = $comment_id;
-	}
-}
-
-function woo_widget() {
-
-	if (!woopra_do_track()) {
-		return;
-	}
-
-	global $woopra_visitor;
-	global $woopra_events;
+<div class="wrap">
+<?php screen_icon(); ?>
+	<h2><?php _e( 'Woopra Settings', 'woopra' ); ?></h2>
+	<p><?php _e('For more info about installation and customization, please visit <a href="http://www.woopra.com/installation-guide">the installation page in your member&#8217;s area', 'woopra') ?></a></p>
 	
+	<form id="woopra_settings_form" method="post" action="options.php">
+	<?php settings_fields('woopra_settings'); ?>
+	
+	<h3><? _e('Main Settings'); ?></h3>
+	<table class="form-table">
+		<tr valign="top">
+			<th scope="row"><?php _e('API Key', 'woopra') ?><small><?php _e('(Optional)', 'woopra') ?></small></th>
+				<td>
+					<input type="text" value="<?php echo attribute_escape( get_option('woopra_api_key') ); ?>" id="apikey" name="apikey"/><br/>
+					<?php _e("You can find the Website's API Key in <a href='http://www.woopra.com/members/'>your member&#8217;s area", 'woopra') ?></a>
+				</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row"><?php _e('Analytics Link Location', 'woopra') ?></th>
+			<td>
+			<?php
+				$woopra_tab_options = array('dashboard' => __("At the dashboard menu", 'woopra'), 'toplevel' => __('At the top level menu', 'woopra'));
+				foreach ( $woopra_tab_options as $key => $value) {
+					$selected = (get_option('woopra_analytics_tab') == $key) ? 'checked="checked"' : '';
+					echo "\n\t<label><input id='$key' type='radio' name='woopratab' value='$key' $selected/> $value</label><br />";
+				}
+			?>
+			</td>
+		</tr>
+	</table>	
+	<br/>
+	<h3><? _e('Tracking Settings'); ?></h3>
+	<table class="form-table">
+		<tr valign="top">
+			<th scope="row"><?php _e('Status', 'woopra') ?></th>
+			<td>
+			<?php
+				$woopra_status_options = array('on' => __("On", 'woopra'), 'off' => __('Off', 'woopra'));
+				foreach ( $woopra_status_options as $key => $value) {
+					$selected = (get_option('woopra_status_option') == $key) ? 'checked="checked"' : '';
+					echo "\n\t<label><input id='$key' type='radio' name='woopra_status' value='$key' $selected/> $value</label><br />";
+				}
+			?>
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row"><?php _e('Ignore Administrator', 'woopra') ?></th>
+			<td>
+				<input type="checkbox" <?php checked('1', get_option('woopra_ignore_admin')); ?> id="ignoreadmin" name="ignoreadmin"/> <label for="ignoreadmin"><?php _e("Ignore Administrator Visits"); ?></label><br /><?php _e("Enable this check box if you want Woopra to ignore your or any other administrator visits."); ?>
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row"><?php _e('Admin Area', 'woopra') ?></th>
+			<td>
+				<input type="checkbox" <?php checked('1', get_option('woopra_track_admin')); ?> id="trackadmin" name="trackadmin"/> <label for="trackadmin"><?php _e("Track Admin Pages"); ?></label><br /><?php printf(__("Admin pages are all pages under %s"), get_option('siteurl')."/wp-admin/" ); ?>
+			</td>
+		</tr>
+	</table>
+	<br/>
+	<h3><? _e('Event Settings'); ?></h3>
+	<table class="form-table">
+		<tr valign="top">
+			<th scope="row"><?php _e('Main Area Events', 'woopra') ?></th>
+			<td>
+			<?php
+				foreach ( $this->woopra_events['main'] as $action => $data) {
+					echo "\n\t<input type=\"checkbox\"" . checked('YES', get_option('woopra_event_' . $action)) . " id=\"" . $action . "\" name=\"woopra_event[".$action."]\"/> <label for=\"woopra_event[".$action."]\">".$data['label']."</label><br />".$data['description'];						
+				}
+			?>				
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row"><?php _e('Admin Area Events', 'woopra') ?></th>
+			<td>
+			<?php
+				foreach ( $this->woopra_events['admin'] as $action => $data) {
+					echo "\n\t<input type=\"checkbox\"" . checked('YES', get_option('woopra_event_' . $action)) . " id=\"" . $action . "\" name=\"woopra_event[".$action."]\"/> <label for=\"woopra_event[".$action."]\">".$data['label']."</label><br />".$data['description'];						
+				}
+			?>
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row"><?php _e('Third Party Events', 'woopra') ?></th>
+			<td>
+			<?php
+				foreach ( $this->woopra_events['custom'] as $action => $data) {
+					echo "\n\t<input type=\"checkbox\"" . checked('YES', get_option('woopra_event_' . $action)) . " id=\"" . $action . "\" name=\"woopra_event[".$action."]\"/> <label for=\"woopra_event[".$action."]\">".$data['label']."</label><br />".$data['description'];						
+				}
+				if (!count($this->woopra_events['custom']))
+					echo "<strong>" . __('No Custom Events Regestiered.') . "</strong>";
+			?>
+			</td>
+		</tr>				
+	</table>
+	</form>
+	
+	<?php }
+
+	/**
+	 * Create the Javascript Code at the Bottom
+	 * @since 1.4.1
+	 * @return 
+	 */
+	function woopra_widget() {
+		$this->debug("woopra_widget()");
 	echo "<!-- Woopra Analytics Code -->\n";
 	echo "<script type=\"text/javascript\">\r\n";
 	echo "var woopra_visitor = new Array();\r\n";
 	echo "var woopra_event = new Array();\r\n";
-
-	if (get_option('woopra_auto_tag_commentators') == 'YES' && $woopra_visitor['name'] != NULL) {
-		echo "woopra_visitor['name'] = '" . js_escape( $woopra_visitor['name'] ) . "';\r\n";
-		echo "woopra_visitor['email'] = '" . js_escape($woopra_visitor['email']) . "';\r\n";
-		echo "woopra_visitor['avatar'] = 'http://www.gravatar.com/avatar.php?gravatar_id=" . md5(strtolower($woopra_visitor['email'])) . "&size=60&default=http%3A%2F%2Fstatic.woopra.com%2Fimages%2Favatar.png';\r\n";
+	
+	if (get_option('woopra_auto_tag_commentators') == 'YES' && $this->woopra_visitor['name'] != null) {
+		echo "woopra_visitor['name'] = '" . js_escape($this->woopra_visitor['name']) . "';\r\n";
+		echo "woopra_visitor['email'] = '" . js_escape($this->woopra_visitor['email']) . "';\r\n";
+		echo "woopra_visitor['avatar'] = 'http://www.gravatar.com/avatar.php?gravatar_id=" . md5(strtolower($this->woopra_visitor['email'])) . "&size=60&default=http%3A%2F%2Fstatic.woopra.com%2Fimages%2Favatar.png';\r\n";
 	}
-
-	if ($woopra_events) {
-		foreach ($woopra_events as $woopra_event_key => $woopra_event_value) {
-			echo "woopra_event['$woopra_event_key'] = '" . js_escape($woopra_event_value) . "';\r\n";
-		}
-	}
-
+		
 	echo "</script>\r\n";
 	echo "<script src=\"http://static.woopra.com/js/woopra.js\" type=\"text/javascript\"></script>";
 	echo "\n<!-- End of Woopra Analytics Code -->";
-
-}
-
-function woopra_do_track() {
-	if (get_option('woopra_ignore_admin') == 'YES' && function_exists('current_user_can') && current_user_can('manage_options')) {
-		return false;
+		
 	}
 
-	
-	return true;
-}
-
-function woopra_add_menu () {
-	if (function_exists('add_menu_page')) {
-		if (get_option('woopra_analytics_tab') && get_option('woopra_analytics_tab') =='toplevel') {
-			add_menu_page(
-				__("Woopra Analytics", 'woopra')
-				, __("Woopra Analytics", 'woopra')
-				, "manage_options"
-				, "woopra_analytics.php"
-				, "woopra_analytics_show_content"); 
-		}
-		else {
-			add_submenu_page(
-				'index.php',
- 				__("Woopra Analytics", 'woopra')
- 				, __("Woopra Analytics", 'woopra')
-				, 'manage_options'
-				, "woopra-analytics"
- 				, "woopra_analytics_show_content");
+	function woopra_detect() {
+		$this->debug("woopra_detect()");
+		global $userdata;	//	Needed if the user is logged in.
+		
+		//	Check to see if the user has a cookie.. if so... get it!
+		$author = str_replace("\"","\\\"",$_COOKIE['comment_author_'.COOKIEHASH]);
+		$email = str_replace("\"","\\\"",$_COOKIE['comment_author_email_'.COOKIEHASH]);
+		if (!empty($author)) {
+			$this->woopra_visitor['name'] = $author;
+			$this->woopra_visitor['email'] = $email;
 		}
 	
+		if (is_user_logged_in()) {
+			get_currentuserinfo();
+			$this->woopra_visitor['name'] = $userdata->user_login;
+			$this->woopra_visitor['email'] = $userdata->user_email;
+		}
 	}
-	if (function_exists('add_options_page')) {
-		 add_options_page(
-		 	__("Woopra Settings", 'woopra')
-		 	, __("Woopra Settings", 'woopra')
-		 	, 7
-		 	, basename(__FILE__)
-		 	, 'woopra_print_admin_html');
+
+	/**
+	 * Compatability for PHP 4.
+	 * @return 
+	 */
+	function Woopra() {
+		$this->__construct();
+	}
+	
+	/**
+	 * Debug code. Has to be turned on for it to work.
+	 * @return 
+	 * @see $this->debug
+	 */
+	function debug($string) {
+		if ($this->debug)
+			echo $string . "<br/>";
 	}
 }
 
-function woopra_print_admin_html() {
-	?>
-	<div class="wrap">
-	
-	<?php
-
-		$smsg = "";
-		if (isset($_POST['submitoptions'])) {
-			check_admin_referer('update-woopra-options');
-			if (isset($_POST['autotag'])) {
-				update_option('woopra_auto_tag_commentators','YES');
-			}
-			else {
-				update_option('woopra_auto_tag_commentators','NO');
-			}
-
-			if (isset($_POST['woopratab'])) {
-				update_option('woopra_analytics_tab',$_POST['woopratab']);
-			}
-
-			if (isset($_POST['trackadmin'])) {
-				update_option('woopra_track_admin','YES');
-			}
-			else {
-				update_option('woopra_track_admin','NO');
-			}
-			
-			if (isset($_POST['ignoreadmin'])) {
-				update_option('woopra_ignore_admin','YES');
-			}
-			else {
-				update_option('woopra_ignore_admin','NO');
-			}
-			
-			if (isset($_POST['showcomments'])) {
-				update_option('woopra_show_comments','YES');
-			}
-			else {
-				update_option('woopra_show_comments','NO');
-			}
-			
-			if (isset($_POST['showsearches'])) {
-				update_option('woopra_show_searches','YES');
-			}
-			else {
-				update_option('woopra_show_searches','NO');
-			}
-			
-			if (isset($_POST['apikey'])) {
-				update_option('woopra_api_key', $_POST['apikey']);
-			}
-			else {
-				update_option('woopra_api_key','');
-			}
-
-			?>
-
-			<div id="message" class="updated fade"><p><?php _e('Settings updated!', 'woopra') ?></p></div>
-
-	<?php } ?>
-        
-	<h2>Woopra Settings</h2>
-	<p><?php _e('For more info about installation and customization, please visit <a href="http://www.woopra.com/installation-guide">the installation page in your member&#8217;s area', 'woopra') ?></a></p>
-	<form action="" method="post">
-	<?php if ( function_exists('wp_nonce_field') ) wp_nonce_field('update-woopra-options'); ?>
-	<table class="form-table">
-		<tr valign="top">
-		<th scope="row"><?php _e('API Key', 'woopra') ?><small><?php _e('(Optional)', 'woopra') ?></small></th>
-		<td>
-		<input type="text" value="<?php echo attribute_escape( get_option('woopra_api_key') ); ?>" id="apikey" name="apikey"/><br/>
-		<?php _e("You can find the Website's API Key in <a href='http://www.woopra.com/members/'>your member&#8217;s area", 'woopra') ?></a>
-		</td>
-		</tr>
-		<tr valign="top">
-		<th scope="row"><?php _e('Show Analytics', 'woopra') ?></th>
-		<td>
-		<?php
-			$woopra_tab_options = array('dashboard' => __("At the dashboard menu", 'woopra'), 'toplevel' => __('At the top level menu', 'woopra'));
-			foreach ( $woopra_tab_options as $key => $value) {
-				$selected = (get_option('woopra_analytics_tab') == $key) ? 'checked="checked"' : '';
-				echo "\n\t<label><input id='$key' type='radio' name='woopratab' value='$key' $selected/> $value</label><br />";
-			}
-		?>
-		</td>
-		</tr>
-		<tr valign="top">
-		<th scope="row"><?php _e('Ignore Administrator', 'woopra') ?></th>
-		<td>
-		<input type="checkbox" <?php checked('YES', get_option('woopra_ignore_admin')); ?> id="ignoreadmin" name="ignoreadmin"/> <label for="ignoreadmin">Ignore Administrator Visits</label><br />Enable this check box if you want Woopra to ignore your or any other administrator visits.
-		</td>
-		</tr>
-		<tr valign="top">
-		<th scope="row"><?php _e('Admin Area', 'woopra') ?></th>
-		<td>
-		<input type="checkbox" <?php checked('YES', get_option('woopra_track_admin')); ?> id="trackadmin" name="trackadmin"/> <label for="trackadmin">Track admin pages</label><br />Admin pages are all pages under <?php echo get_option('siteurl'); ?>/wp-admin/
-		</td>
-		</tr>
-		<tr valign="top">
-		<th scope="row"><?php _e('Auto Tagging', 'woopra') ?></th>
-		<td>
-		<input type="checkbox" <?php checked('YES', get_option('woopra_auto_tag_commentators')); ?> id="autotag" name="autotag"/> <label for="autotag"><?php _e('Automatically tag members &amp; commentators', 'woopra') ?></label>
-		</td>
-		</tr>
-		<tr valign="top">
-		<th scope="row"><?php _e('Show Comments', 'woopra') ?></th>
-		<td>
-		<input type="checkbox" <?php checked('YES', get_option('woopra_show_comments')); ?> id="showcomments" name="showcomments"/> <label for="showcomments"><?php _e('Show comments as they are posted.', 'woopra') ?></label><br /><?php _e('You will see an excerpt of the comment in the Woopra Live section', 'woopra') ?>
-		</td>
-		</tr>
-		<tr valign="top">
-		<th scope="row"><?php _e('Show Searches', 'woopra') ?></th>
-		<td>
-		<input type="checkbox" <?php checked('YES', get_option('woopra_show_searches')); ?> id="showsearches" name="showsearches"/> <label for="showsearches"><?php _e('Show search queries.', 'woopra') ?></label>
-		</td>
-		</tr>
-	</table>
-	<p class="submit"><input type="submit" name="submitoptions" value="<?php _e('Save Changes', 'woopra') ?>" /></p>
-	</form>
-	</div>
-	
-<?php 
-}
-
-
-function woopra_analytics_head() {
-	echo "<script src=\"". get_option('siteurl') ."/wp-content/plugins/woopra/woopra_analytics.js?1\"></script>\r\n";
-	echo "<script src=\"". get_option('siteurl') ."/wp-content/plugins/woopra/swfobject.js\"></script>\r\n";
-	echo "<script src=\"". get_option('siteurl') ."/wp-content/plugins/woopra/datepicker.js\"></script>\r\n";
-	echo "<link rel='stylesheet' href='". get_option('siteurl') ."/wp-content/plugins/woopra/woopra_analytics.css' type='text/css' />";
-	echo "<link rel='stylesheet' href='". get_option('siteurl') ."/wp-content/plugins/woopra/datepicker.css' type='text/css' />";
-}
-
-include 'woopra_analytics.php';
-
-add_action('admin_menu', 'woopra_add_menu');
-add_action('template_redirect', 'woo_detect');
-add_action('template_redirect', 'woo_session_start');
-add_action('comment_post', 'woo_comment');
-add_action('admin_print_scripts', 'woopra_analytics_head');
-add_action('wp_footer', 'woo_widget', 10);
-
-
-if (get_option('woopra_track_admin') == 'YES') {
-	add_action('admin_footer', 'woo_widget');
-}
-
+// Start this plugin once all other plugins are fully loaded
+add_action( 'init', 'Woopra_Init' ); function Woopra_Init() { global $Woopra; $Woopra = new Woopra(); }
 
 ?>
