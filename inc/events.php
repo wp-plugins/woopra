11a70e1a97bf3ddb035daac3d9d6ec81322e8a3d
ShaneF <ShaneF@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-03 05:08:56 +0000

    initial creation
    git-svn-id: https://plugins.svn.wordpress.org/woopra/trunk@131678 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/events.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * WoopraEvents Class for Woopra
+ *
+ * This class contains all event related code including the API for other plugins to use.
+ *
+ * @since 1.4.1
+ * @package woopra
+ * @subpackage events
+ */
+class WoopraEvents extends Woopra {
+
+	/**
+	 * PHP 4 Style constructor which calls the below PHP5 Style Constructor
+	 * @since 1.4.1
+	 * @return none
+	 */
+	function WoopraEvents () {
+		$this->__construct();
+	}
+
+	/**
+	 * Woopra Events
+	 * @since 1.4.1
+	 * @return none
+	 * @constructor
+	 */
+	function __construct() {
+		
+	}
+
+}
+
+?>
